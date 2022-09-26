812501a075f603f99dd78de828f49035dcf057b9
Ngo Nhat Tri <nhattri1503@gmail.com>
2022-09-26 11:38:20 +0900

    Postのモデル・コントロール作成

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+    protected $fillable = ['user_id', 'name', 'contents'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
