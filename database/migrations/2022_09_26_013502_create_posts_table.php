5caeb1f044499ba5c3a2ba7017b52ce695fbe065
ngonhattri <nhattri1503@gmail.com>
2022-11-18 18:27:10 +0900

    changed migration of post

--- database/migrations/2022_09_26_013502_create_posts_table.php
+++ database/migrations/2022_09_26_013502_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigInteger('id');
-            $table->bigInteger('user_id');
+            $table->id();
+            $table->integer('user_id');
             $table->ipAddress('name');
             $table->text('contents');
             $table->timestamps();
